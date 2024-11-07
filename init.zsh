8d395e563315b0193f375f2a83d27d93b249a5ea
Eric Nielsen <4120606+ericbn@users.noreply.github.com>
2024-11-06 19:45:13 -0500

    Fail silently if direnv is not installed

--- init.zsh
+++ init.zsh
@@ -1,10 +1,11 @@
-if [[ -z ${NO_COLOR} && ${+DIRENV_LOG_FORMAT} -eq 0 ]] export DIRENV_LOG_FORMAT=$'\E[2mdirenv: %s\E[0m'
 () {
   local -r target=${1}
   shift
+  (( ${+commands[${1}]} )) || return 1
   if [[ ! ( -s ${target} && ${target} -nt ${commands[${1}]} ) ]]; then
     "${@}" >! ${target} || return 1
     zcompile -UR ${target}
   fi
   source ${target}
-} ${0:h}/direnv-hook-zsh.zsh direnv hook zsh
+} ${0:h}/direnv-hook-zsh.zsh direnv hook zsh || return 1
+if [[ -z ${NO_COLOR} && ${+DIRENV_LOG_FORMAT} -eq 0 ]] export DIRENV_LOG_FORMAT=$'\E[2mdirenv: %s\E[0m'
